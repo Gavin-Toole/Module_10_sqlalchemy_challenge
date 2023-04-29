30b0155eac654ceb3e2a056ed6da19ed5dae3d62
Gavin-Toole <121550462+Gavin-Toole@users.noreply.github.com>
2023-04-29 09:07:03 -0230

    final part 1

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,11 +1,24 @@
 # Import the dependencies.
+import datetime as dt
+import numpy as np
+import pandas as pd
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+# engine = create_engine("sqlite:///titanic.sqlite")
+# Base = automap_base()
+# Base.prepare(autoload_with=engine)
+# print(Base.classes.keys())
+# Passanger = Base.classes.passanger
 
 # reflect an existing database into a new model
 
