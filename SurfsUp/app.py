11e2b50e47870c50cec3ff8d4ebad2f945270d04
Gavin-Toole <121550462+Gavin-Toole@users.noreply.github.com>
2023-04-30 16:30:43 -0230

    ready for review

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -49,8 +49,8 @@ def welcome():
         f"Precipitation measurementsf for hte last 12 months: /api/v1.0/precipitation<br/>"
         f"A list of stations from the database: /api/v1.0/stations<br/>"
         f"A list of dates and temperature observations for the previous year: /api/v1.0/tobs<br/>"
-        f"Enter a Start date to view the min, max and average temperatures: /api/v1.0/<start><br/>"
-        f"Enter a start and end date to view the min, max and average temperatures: /api/v1.0/<start><end><br/>"
+        f"Enter a Start date to view the min, max and average temperatures(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd<br/>"
+        f"Enter a start and end date to view the min, max and average temperatures(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd/yyyy-mm-dd"
     )
 
 # Create first route precipitation data for the last 12 months.
@@ -124,31 +124,49 @@ def tobs():
     # return jason list of dictionary
     return jsonify(tobs)
 
-# @app.route("/api/v1.0/<start>")
-# def start():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-    
-#     # Close out session (link) from Python to the DB
-#     session.close()
-    
-#     # Create dictionary for results
-    
+@app.route("/api/v1.0/<start>")
+def get_t_start(start):
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-#     # return jason list of dictionaryreturn jsonify
+    queryresult = session.query(func.min(Measurements.tobs), func.round(func.avg(Measurements.tobs),2),\
+     func.round(func.max(Measurements.tobs),2)).filter(Measurements.date >= start).all()
+    
+    # Close out session (link) from Python to the DB
+    session.close()
 
-# @app.route("/api/v1.0/<start><end>")
-# def startend():
-#     # Create our session (link) from Python to the DB
-#     session = Session(engine)
-    
-#     # Close out session (link) from Python to the DB
-#     session.close()
-    
-#     # Create dictionary for results
-    
+    tobsall = []
+    for min,avg,max in queryresult:
+        tobs_dict = {}
+        tobs_dict["Min"] = min
+        tobs_dict["Average"] = avg
+        tobs_dict["max"] = max
+        tobsall.append(tobs_dict)
 
-#     # return jason list of dictionaryreturn jsonify
+    return jsonify(tobsall)
+
+
+@app.route('/api/v1.0/<start>/<end>')
+def get_t_start_end(start,end):
+   
+    # Close out session (link) from Python to the DB
+    session = Session(engine)
+   
+    queryresult = session.query(func.min(Measurements.tobs), func.round(func.avg(Measurements.tobs),2), func.max(Measurements.tobs)).\
+        filter(Measurements.date >= start).filter(Measurements.date <= end).all()
+   
+    # Close out session (link) from Python to the DB
+    session.close()
+
+    tobsall = []
+    for min,avg,max in queryresult:
+        tobs_dict = {}
+        tobs_dict["Min"] = min
+        tobs_dict["Average"] = avg
+        tobs_dict["Max"] = max
+        tobsall.append(tobs_dict)
+
+    return jsonify(tobsall)
 
 
 if __name__ == "__main__":
